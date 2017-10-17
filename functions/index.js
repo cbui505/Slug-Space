ff3383e5a73076a2bef875a331a0d743d89130aa
Christopher Bui <christopherbui525@gmail.com>
2017-10-17 15:15:31 -0700

    set up initial firebase and add necessary files

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
+const engines = require('consolidate');
+
+const firebaseApp = firebase.initializeApp(
+	functions.config().firebase
+	);
+
+const app = express();
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.get('/', (request, response) => {
+	response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+	response.render('index', {});
+});
+
+exports.app = functions.https.onRequest(app);
