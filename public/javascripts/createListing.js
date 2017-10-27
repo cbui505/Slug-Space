b3fac80d27a9dab36a25b02c46cc237f37ad7b8a
Christopher Bui <christopherbui525@gmail.com>
2017-10-27 00:27:44 -0700

    parse address as coordinates in Santa Cruz area

--- public/javascripts/createListing.js
+++ public/javascripts/createListing.js
@@ -4,6 +4,8 @@ var createListing = (function(){
         bindCreateButton();
     }
 
+    geocoder = new google.maps.Geocoder();
+
     function bindCreateButton(){
         //object that will store user input as attributes
         var listing;
@@ -28,8 +30,20 @@ var createListing = (function(){
             listing.fee = $fee.val() ? $fee.val() : 0;
             listing.desciption = $description.val() ? $description.val() : "No description provided" ;
 
-            /* TODO: use google maps API to parse address as coordinates */
-            listing.coordinates = "Coming soon...";
+            //parse input address as latitude,longitude coordinates            
+            var coordinates = [];
+            //restrict address to Santa Cruz area
+            geocoder.geocode( { 'address' : listing.address + ", Santa Cruz, CA"}, function( results, status ) {
+                if( status == google.maps.GeocoderStatus.OK ) {
+                    coordinates.push(results[0].geometry.location.lat());
+                    coordinates.push(results[0].geometry.location.lng());
+                } else {
+                    alert( 'Failed to geocode address with error: ' + status );
+                }
+            } );
+            
+            //add coordinates array to listing. Warning: could be empty
+            listing.coordinates = coordinates;
 
             //for now, print to console
             console.log(listing);
